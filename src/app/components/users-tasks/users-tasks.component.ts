a803fc2f65b69b61c50e20d6724d6e3a203da20b
Guillermo Mata <guillemat@gmail.com>
2022-06-14 11:00:07 -0400

    Users in a table

---
+++ src/app/components/users-tasks/users-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-tasks',
+  templateUrl: './users-tasks.component.html',
+  styleUrls: ['./users-tasks.component.css']
+})
+export class UsersTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import { IUser } from 'src/app/interfaces/interfaces';
+
 
 @Component({
   selector: 'app-users',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'username', 'email', 'phone'];
+  users: IUser[] = [];
+  dataSource = this.users;
 
-  constructor() { }
+  constructor(
+    private userService: UsersService,
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe( resp => {
+      this.users = resp;
+    })
   }
 
 }
